c5eebf1f69c206b5c2eb52552b03762399c7ff35
sohanshrestha12 <sohanshrestha40@gmail.com>
2023-11-23 21:16:58 +0545

    User role

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 const router = Router();
 import { register,login } from "../controllers/authController.js";
+import { validateRegisterInput } from "../middlewares/validationMiddleware.js";
 
-router.post('/register',register)
+router.post("/register", validateRegisterInput, register);
 router.post('/login',login)
 
 export default router;
