588383ce3e0448991c229e732dbf7cdcca397c93
sohanshrestha12 <sohanshrestha40@gmail.com>
2023-11-25 21:10:34 +0545

    update user

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getApplicationStats, getCurrentUser, updateUser } from "../controllers/userController.js";
+import { validateUpdateUserInput } from "../middlewares/validationMiddleware.js";
 const router = Router();
 
 router.get('/current-user',getCurrentUser);
 router.get('/admin/app-stats',getApplicationStats);
-router.patch('/update-user',updateUser);
+router.patch('/update-user',validateUpdateUserInput,updateUser);
 
 
 export default router;
