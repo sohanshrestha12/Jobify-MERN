2cbf4d349a1ed6f6122e9bff0644776d26034332
sohanshrestha12 <sohanshrestha40@gmail.com>
2023-11-25 21:28:04 +0545

    application stats

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getApplicationStats, getCurrentUser, updateUser } from "../controllers/userController.js";
 import { validateUpdateUserInput } from "../middlewares/validationMiddleware.js";
+import { authorizePermissions } from "../middlewares/authMiddleware.js";
 const router = Router();
 
 router.get('/current-user',getCurrentUser);
-router.get('/admin/app-stats',getApplicationStats);
+router.get('/admin/app-stats',[authorizePermissions('admin')],getApplicationStats);
 router.patch('/update-user',validateUpdateUserInput,updateUser);
 
 
