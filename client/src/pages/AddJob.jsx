286c5d204cf1c1a45868b9c00ae4763827d8b9e8
sohanshrestha12 <sohanshrestha40@gmail.com>
2023-11-29 21:55:00 +0545

    add job structure

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Wrapper from "../assets/wrappers/DashboardFormPage";
 import { useOutletContext } from "react-router-dom";
 import { Form, useNavigation, redirect } from "react-router-dom";
-import { FormRow,FormRowSelect } from "../components";
+import { FormRow, FormRowSelect } from "../components";
 import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
 
 const AddJob = () => {
@@ -15,14 +15,33 @@ const AddJob = () => {
       <Form method="post" className="form">
         <h4 className="form-title">add job</h4>
         <div className="form-center">
-            <FormRow type="text" name='position' />
-            <FormRow type="text" name='company' />
-            <FormRow type="text" labelText='job location' name='jobLocation' defaultValue={user.location} />
-            <FormRowSelect labelText='job status' name='jobStatus' defaultValue={JOB_STATUS.PENDING} list={Object.values(JOB_STATUS)} />
-            <FormRowSelect labelText='job type' name='jobType' defaultValue={JOB_TYPE.FULL_TIME} list={Object.values(JOB_TYPE)} />
-            <button type="submit" className="btn btn-block form-btn" disabled={isSubmitting}>
-                {isSubmitting?'submitting...':'submit'}
-            </button>
+          <FormRow type="text" name="position" />
+          <FormRow type="text" name="company" />
+          <FormRow
+            type="text"
+            labelText="job location"
+            name="jobLocation"
+            defaultValue={user.location}
+          />
+          <FormRowSelect
+            labelText="job status"
+            name="jobStatus"
+            defaultValue={JOB_STATUS.PENDING}
+            list={Object.values(JOB_STATUS)}
+          />
+          <FormRowSelect
+            labelText="job type"
+            name="jobType"
+            defaultValue={JOB_TYPE.FULL_TIME}
+            list={Object.values(JOB_TYPE)}
+          />
+          <button
+            type="submit"
+            className="btn btn-block form-btn"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "submitting..." : "submit"}
+          </button>
         </div>
       </Form>
     </Wrapper>
