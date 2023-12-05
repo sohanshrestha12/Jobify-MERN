006968bf6cf0e88e99a6e9a7ec8a3839e7d96437
sohanshrestha12 <sohanshrestha40@gmail.com>
2023-12-05 16:48:22 +0545

    cloudinary

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 import morgan from "morgan";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
+
 
 //routers
 import jobRouter from "./routes/jobRouter.js";
@@ -21,6 +23,13 @@ import path from "path";
 //middlewares
 import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware.js";
 import { authenticateUser } from "./middlewares/authMiddleware.js";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
 
 const __dirname =dirname(fileURLToPath(import.meta.url));
 
