61774f769fd9ff8c936af520b23a4fbbb41317d8
sohanshrestha12 <sohanshrestha40@gmail.com>
2023-12-05 21:18:20 +0545

    populate mockData

---
+++ populate.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+import User from "./models/UserModel.js";
+import Job from "./models/JobModel.js";
+import {readFile} from 'fs/promises'
+
+try {
+    await mongoose.connect(process.env.MONGO_URL);
+    const user = await User.findOne({ email: "abc@gmail.com" });
+    // const user = await User.findOne({email:'test@test.com'});
+    const jsonJobs =JSON.parse(await readFile(new URL('./utils/mockData.json',import.meta.url)));
+    const jobs = jsonJobs.map((job)=>{
+        return {...job,createdBy:user._id}
+    });
+    await Job.deleteMany({createdBy:user._id});
+    await Job.create(jobs);
+    console.log('success');
+    process.exit(0);
+} catch (error) {
+    console.log(error);
+    process.exit(1);
+    
+}
