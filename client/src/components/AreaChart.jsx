63e20e6bbfd4c0f69c28f7d77b9dc1b02b65a47a
sohanshrestha12 <sohanshrestha40@gmail.com>
2023-12-06 21:23:46 +0545

    AreaChart

--- client/src/components/AreaChart.jsx
+++ client/src/components/AreaChart.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import {
+  ResponsiveContainer,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  AreaChart,
+} from "recharts";
 
-const AreaChart = () => {
+const AreaChartComponent = ({ data }) => {
   return (
-    <div>AreaChart</div>
-  )
-}
+    <ResponsiveContainer width="100%" height={300}>
+      <AreaChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey='date'/>
+        <YAxis allowDecimals={false}/>
+        <Tooltip/>
+        <Area type='monotone' dataKey='count' stroke='#2cb1bc' fill='#bef8fd'/>
+      </AreaChart>
+    </ResponsiveContainer>
+  );
+};
 
-export default AreaChart
+export default AreaChartComponent;
