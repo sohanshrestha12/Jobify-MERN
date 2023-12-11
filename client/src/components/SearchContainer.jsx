d3fae89ebd27a30659f6604042963335c17bc81c
sohanshrestha12 <sohanshrestha40@gmail.com>
2023-12-11 16:16:20 +0545

    debounce

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -8,7 +8,19 @@ import { useAllJobsContext } from '../pages/AllJob'
 const SearchContainer = () => {
   const {searchValues} = useAllJobsContext();
   const {search,jobStatus,jobType, sort} = searchValues;
-  const submit = useSubmit()
+  const submit = useSubmit();
+
+  const debounce = (onChange) =>{
+    let timeout;
+    return (e)=>{
+      const form = e.currentTarget.form;
+      clearTimeout(timeout);
+      timeout = setTimeout(()=>{
+        onChange(form);
+      },2000)
+
+    }
+  }
   return (
     <Wrapper>
       <Form className="form">
@@ -18,9 +30,9 @@ const SearchContainer = () => {
             type="search"
             name="search"
             defaultValue={search}
-            onChange={(e) => {
-              submit(e.currentTarget.form);
-            }}
+            onChange={debounce((form)=>{
+              submit(form);
+            })}
           />
           <FormRowSelect
             labelText="job status"
